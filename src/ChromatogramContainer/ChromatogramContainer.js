a4564c6a233c11b7c590d08a92f93e83957556bc
Bryan McKernon <bryanmckernon@gmail.com>
2021-01-15 19:38:51 -0800

    Graph seems to be working

--- src/ChromatogramContainer/ChromatogramContainer.js
+++ src/ChromatogramContainer/ChromatogramContainer.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './ChromatogramContainer.scss';
+import Chart from 'chart.js';
 
 const ChromatogramContainer = (props) => {
-    console.log(props.ChromatogramData)
+    
+    let x_coordinates = props.ChromatogramData.x;
+    let y_coordinates = props.ChromatogramData.y;
+
+    let coordinates = [];
+
+    for (let x in x_coordinates) {
+        coordinates.push({x: x_coordinates[x], y: y_coordinates[x]})
+    }
+
+    let Canvas = useRef(null);
+
+    useEffect(() => {    
+        let CanvasContext = Canvas.current.getContext('2d');
+        new Chart(CanvasContext, {
+            type: 'scatter',
+            data: {
+                labels: [],
+                datasets:[{
+                    label: 'Data',
+                    data: coordinates
+                }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                      scaleLabel: {
+                        display: true,
+                        labelString: props.ChromatogramData.x_units
+                      }
+                    }],
+                    yAxes: [{
+                        scaleLabel: {
+                          display: true,
+                          labelString: props.ChromatogramData.y_units
+                        }
+                      }]                      
+                  } 
+            }
+        });
+    });
+    
     return (
         <div className="ChromatogramContainer">
-            <h1>chromatogram</h1>
+            <canvas ref={Canvas} className="Chromatogram"></canvas>
         </div>
     );
 };
